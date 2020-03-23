32254981319ad473eb9dbb61255aafe56ada4590
Filip Varga <filipvarga89@gmail.com>
2020-03-23 13:58:00 +0100

    support for latest t-rex with custom patches
    Signed-off-by: Filip Varga <filipvarga89@gmail.com>

---
+++ tests/http_custom.py
@@ -0,0 +1,25 @@
+from trex_astf_lib.api import *
+
+
+class Prof1():
+    cps = 1
+
+    def __init__(self):
+        pass
+
+    def get_profile(self, **kwargs):
+        # ip generator
+        ip_gen_c = ASTFIPGenDist(ip_range=["16.0.0.8", "16.0.0.20"], distribution="seq")
+        ip_gen_s = ASTFIPGenDist(ip_range=["48.0.0.0", "48.0.0.255"], distribution="seq")
+        ip_gen = ASTFIPGen(glob=ASTFIPGenGlobal(ip_offset="1.0.0.0"),
+                           dist_client=ip_gen_c,
+                           dist_server=ip_gen_s)
+
+        return ASTFProfile(default_ip_gen=ip_gen,
+                            cap_list=[ASTFCapInfo(file="../avl/delay_10_http_browsing_0.pcap",
+                            cps=self.cps)])
+
+
+def register():
+    return Prof1()
+
