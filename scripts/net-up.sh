ed76da3ec7b6533514a6a6183843cb4d91751ab8
Filip Varga <filipvarga89@gmail.com>
2020-05-04 13:37:41 +0200

    cleaning up repository
    Signed-off-by: Filip Varga <filipvarga89@gmail.com>

--- scripts/net-up.sh
+++ scripts/net-up.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # veth pair 1 (inside network)
-sudo ip link del dev vpp0 &> /dev/null
-sudo ip link add vpp0 type veth peer name vpp1
-sudo ip addr add 10.0.0.1/24 dev vpp0 &> /dev/null
-sudo ethtool -K vpp0 rx off tx off # ??
-sudo ip link set dev vpp0 up
+ip link del dev vpp0 &> /dev/null
+ip link add vpp0 type veth peer name vpp1
+ip addr add 10.0.0.1/24 dev vpp0 &> /dev/null
+ethtool -K vpp0 rx off tx off # ??
+ip link set dev vpp0 up
 # veth pair 2 (outside network)
-sudo ip link del dev vpp2 &> /dev/null
-sudo ip link add vpp2 type veth peer name vpp3
-sudo ip addr add 11.0.0.1/24 dev vpp2 &> /dev/null
-sudo ethtool -K vpp2 rx off tx off # ??
-sudo ip link set dev vpp2 up
+ip link del dev vpp2 &> /dev/null
+ip link add vpp2 type veth peer name vpp3
+ip addr add 11.0.0.1/24 dev vpp2 &> /dev/null
+ethtool -K vpp2 rx off tx off # ??
+ip link set dev vpp2 up
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,8 @@ docker run -it -d --name trex-run \
   --privileged --cap-add=ALL --network=host \
   vpp-tests/trex:latest
 
-docker rm -f vpp-run &> /dev/null
+#docker rm -f vpp-run &> /dev/null
 # TODO: (change your docker run command for creating vpp container)
-docker run -it -d --name vpp-run \
-  --privileged --cap-add=ALL --network=host \
-  vpp-tests/vpp:latest
+#docker run -it -d --name vpp-run \
+#  --privileged --cap-add=ALL --network=host \
+#  vpp-tests/vpp:latest
